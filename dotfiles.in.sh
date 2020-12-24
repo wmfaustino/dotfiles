54e27e283661e2ec73e54873cfefdea2efb8fa88
wmfaustino <wfaustino.dev@gmail.com>
2020-12-24 01:20:46 -0300

    rewriting from scratch

--- dotfiles.in.sh
+++ dotfiles.in.sh
@@ -13,45 +13,73 @@
 #------------------------------------------------------+
 #
 
-# ------- VARIABLES
-            # each directory have dotfiles inside
-	    # src dest
 declare -Arg dirs=(
-    ["${PWD}"/.config]="${HOME}/.config"
-    ["${PWD}"/home]="${HOME}"
+  ["$PWD/home"]="$HOME"
+  ["$PWD/.config"]="$HOME/.config"
 )
 
-# ------- FUNCTIONS
-function installDotfiles()(
+# echo "${src_dirs[$PWD/home]}"
 
+
+_install_dotfiles()(
   shopt -s dotglob
-                     # dir keys: .config and home
-  for i in "${!dirs[@]}"; do
-     
-    makeSymlink "$i"/* # * are files and directories
-    
-  done
 
-  exit 0
+  for src in ${!dirs[@]}; do
+    for dotfile in $src/*; do
+      
+      echo $src/${dotfile##*/}
+      echo "${dirs[$src]}/${dotfile##*/}"
+      echo
+    done
+  done
 )
 
-function makeSymlink(){
-           
-  for i in "${@}"; do
- 
-    src="${i}"
-    dest="${dirs[${i%/*}]}" #/${i##*/}"
-    
-    ln -sfv "${src}" "${dest}"
+_install_dotfiles 
+
+#for i in "${@}"; do
+#
+#  src_src_dirs="${i}"
+#  dest="${src_dirs[${i%/*}]}" #/${i##*/}"
+#    
+#  ln -sfv "${src_src_dirs}" "${dest}"
    
    # ln -s ~/.local/share/dotfiles/.config/i3 ~/.config
    #ln -sfv [target file] [Symbolic filename]
  
 #ln [OPTION]... -t DIRECTORY TARGET...
-done
+#done
+# ------- FUNCTIONS
+#function installDotfiles()(
+#
+#  shopt -s dotglob
+#                     # src_dir keys: .config and home
+#  for i in "${!src_dirs[@]}"; do
+#     
+#    #makeSymlink "$i"/* # * are files and src_directories
+#    echo "$i"/* # * are files and src_directories
+#    
+#  done
+#
+#  exit 0
+#)
 
-  return 0
-}
+#function makeSymlink(){
+#           
+#  for i in "${@}"; do
+# 
+#    src_src_dirs="${i}"
+#    dest="${src_dirs[${i%/*}]}" #/${i##*/}"
+#    
+#    ln -sfv "${src_src_dirs}" "${dest}"
+#   
+#   # ln -s ~/.local/share/dotfiles/.config/i3 ~/.config
+#   #ln -sfv [target file] [Symbolic filename]
+# 
+##ln [OPTION]... -t DIRECTORY TARGET...
+#done
+#
+#  return 0
+#}
 
 # ENTRY POINT ---------------------------------------------
-installDotfiles
+#installDotfiles
